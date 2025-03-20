e545a88dd0e20d3aaf529a6697f30aae4c2db872
giorgi <giorgi.sikh@outlook.com>
2025-03-20 15:13:49 +0400

    add user services #13

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,24 @@
-import { Controller,Get } from "@nestjs/common";
+import { Controller,Get, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
+
 
 //  /users
 @Controller('users')
 export class UsersController{
 
-@Get()
-Hello(): string {
-return 'users';
-}    
+    @Get()
+    getUsers(){
+        const usersService = new UsersService();
+        return usersService.getAllUsers();
+    }
+
+    @Post()
+    createUser(){
+        const user={id: 3, name:'mery', age: 23, gender: 'female', isMarried: false};
+        const usersService = new UsersService();
+        usersService.CreateUser(user);
+        
+        return 'A new User Created';
+    }
+
 }
---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+export class UsersService{
+    users: {id: number, name: string, age: number, gender: string, isMarried: boolean}[]=[
+        {id: 1, name: 'John', age: 28, gender: 'male', isMarried: false},
+        {id: 2, name: 'Mark', age: 32, gender: 'male', isMarried: true}
+    ]
+
+    getAllUsers(){
+        return this.users;
+    }
+
+    getUserById(id: number){
+        return this.users.find(x=>x.id=id);
+    }
+
+    CreateUser(user: {id: number, name: string, age: number, gender: string, isMarried: boolean}){
+     this.users.push(user);
+        
+    }
+}
