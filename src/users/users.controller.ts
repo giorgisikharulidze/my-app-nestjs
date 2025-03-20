92087770c4071dbee299b04a5d5ffef48b05f3fe
giorgi <giorgi.sikh@outlook.com>
2025-03-20 16:08:35 +0400

    add search by query #14

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Get, Param, Post } from "@nestjs/common";
+import { Controller,Get, Param, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 
@@ -7,9 +7,13 @@ import { UsersService } from "./users.service";
 export class UsersController{
 
     @Get()
-    getUsers(){
+    getUsers(@Query() query: any){
         const usersService = new UsersService();
+        if(query.gender){
+            return usersService.getAllUsers().filter(u=> u.gender === query.gender);
+        }
         return usersService.getAllUsers();
+        
     }
 
     @Get(':id')
